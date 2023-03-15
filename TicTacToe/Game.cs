e200189af8a76c80aa3dba33ae3d2a7ea1634d8c
MKinga08 <dr.m.kinga08@gmail.com>
2023-03-15 11:59:52 +0100

    Creating new classes

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,14 @@
+﻿using Program;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        public List<Player> Players { get; set; }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,20 +1,25 @@
 ﻿using Program;
+using System.Data.SqlTypes;
 using System.Net.WebSockets;
+using System.Threading.Tasks.Dataflow;
+using System.Xml.Linq;
 
 namespace TicTacToe
 {
     class Program
     {
 
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         public static void Main(string[] args)
         {
             Board();
-            GetPlayer();
+            var player1 = new Player("scb",'Q');
+            player1.ChangeSymbol('k');
+           
         }
         public static void Board()
         {
-            char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
+            
             Console.WriteLine("     |     |      ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
             Console.WriteLine("_____|_____|_____ ");
@@ -25,32 +30,10 @@ namespace TicTacToe
             Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
             Console.WriteLine("     |     |      ");
         }
-        public static void GetPlayer()
+        
+        public void CheckWinner()
         {
-            Console.WriteLine("Player1 name?");
-            string name1 = Console.ReadLine()!;
-            Console.WriteLine("Player2 name?");
-            string name2 = Console.ReadLine()!;
-            Console.WriteLine($"Which character would you like to be {name1}? X or O?");
-            int x = Console.Read();
-            char symbol1 = Convert.ToChar(x);
-            //symbol1.ToUpper();
-            char symbol2;
-            if (symbol1 == 'X')
-            {
-                symbol2 = 'O';
-            }
-            else
-            {
-                symbol2 = 'X';
-            }
-            Player player1 = new(name1, symbol1);
-            Player player2 = new(name2, symbol2);
-            Console.WriteLine(name1);
-            Console.WriteLine(name2);
-            Console.WriteLine(symbol1);
-            Console.WriteLine(symbol2);
+        
         }
-
     }
 }
